8060396950eaa46eb0f1f797162c33beb2a47dfc
Ganesh <gp302999@gmail.com>
2024-02-07 15:02:48 +0530

    first commit

--- Test4.java
+++ Test4.java
@@ -1,6 +1,28 @@
 package in.ganesh.test;
 
 public class Test4 {
+
+//    Prob1: A shop sells a candy for Rs 2 per piece and there is an offer that you can claim a candy for 4 wrappers.
+//    Given an initial amount to you, calculate how many candies can you buy from that money
+    int candy = 2;
+    int wrapper = 4;
+    int getMaxCandies(int amount){
+        // multiple of 2's return some candies
+        // multiple of 4's wrappers return 1 candies
+//        if(amount%2==0){
+//            System.out.println("2 Rs one candies...");
+//        }else if(wrapper%4==0){
+//            System.out.println("multiple of 4 wrapper one candies purchase.. ");
+//        }else {
+//            System.out.println("you are not able to purchase candies without amount or wrapper..");
+//        }
+//        return amount;
+
+
+        while (  )
+
+
+    }
     public static void main(String[] args) {
 
     }
